6a28bbb99023a905632828ada79925f9334f1652
Tbolen23 <tbolen23@users.noreply.github.com>
2025-11-10 05:17:28 +0000

    v2.0.1: Fix Arma 3 file paths for proper mod loading
    CRITICAL FIX for mod loading issues:
    
    Changes:
    - Update $PBOPREFIX$ from 'addons\ace_damage_mod' to 'z\ace_dmg\addons\ace_damage_mod'
    - Update config.cpp file path from '\addons\...' to '\z\ace_dmg\addons\ace_damage_mod\functions'
    - Bump version to 2.0.1
    - Update README with version history
    
    Why this matters:
    - The 'z\' prefix is the Arma 3 standard mod namespace format
    - ALL major Arma 3 mods use this format (ACE, CBA, LambsDanger, etc.)
    - Without it, Arma 3 cannot locate files in the PBO
    - This structure was verified against LambsDanger's proven implementation
    
    Fixes:
    - 'File not found' errors when loading functions
    - 'Unexpected modDIR' warnings
    - 'Unexpected workshopID' warnings
    - Functions not being registered with CfgFunctions
    
    The mod will now load correctly and all functions will be accessible.

--- addons/ace_damage_mod/config.cpp
+++ addons/ace_damage_mod/config.cpp
@@ -7,9 +7,9 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion = 1.0;
         requiredAddons[] = {"ace_medical", "ace_medical_damage"};
-        version = "2.0.0";
-        versionStr = "2.0.0";
-        versionAr[] = {2,0,0};
+        version = "2.0.1";
+        versionStr = "2.0.1";
+        versionAr[] = {2,0,1};
     };
 };
 
@@ -17,7 +17,7 @@ class CfgFunctions {
     class ace_damage_mod {
         tag = "ace_dmg";
         class functions {
-            file = "\addons\ace_damage_mod\functions";
+            file = "\z\ace_dmg\addons\ace_damage_mod\functions";
             class init {
                 postInit = 1;
             };
